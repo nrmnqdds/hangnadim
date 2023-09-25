864c3ea2c095bc4c77fc252e6ed03ab786efe9e1
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 22:28:12 +0800

    :lipstick: Creates beautiful landing page

---
+++ app/sections/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import localFont from "next/font/local";
+
+const warGate = localFont({ src: "../../public/WarGate-Normal.otf" });
+
+const HomeScreen = () => {
+  return (
+    <section className="isolate w-full h-screen flex items-center justify-center">
+      <div className="fixed text-center">
+        <h1
+          className={`text-8xl text-slate-100 drop-shadow-glow ${warGate.className}`}
+        >
+          HANG NADIM
+        </h1>
+        <p
+          className={`text-5xl text-gray-500 drop-shadow-xl ${warGate.className}`}
+        >
+          TANGKAS BERANI
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HomeScreen;
