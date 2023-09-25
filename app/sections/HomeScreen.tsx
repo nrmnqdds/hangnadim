93e7f39691eaf0113a12bf4ce6eb22d308f952b7
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 23:02:18 +0800

    :lipstick: Add transition to landing page

--- app/sections/HomeScreen.tsx
+++ app/sections/HomeScreen.tsx
@@ -1,22 +1,29 @@
 import localFont from "next/font/local";
+import { RevealList } from "next-reveal";
 
 const warGate = localFont({ src: "../../public/Wargate-Normal.otf" });
 
 const HomeScreen = () => {
   return (
     <section className="isolate w-full h-screen flex items-center justify-center">
-      <div className="fixed text-center">
+      <RevealList
+        interval={200}
+        delay={200}
+        easing="ease-in-out"
+        origin="bottom"
+        className="fixed text-center invisible"
+      >
         <h1
-          className={`text-8xl text-slate-100 drop-shadow-glow ${warGate.className}`}
+          className={`text-5xl md:text-8xl text-slate-100 drop-shadow-glow ${warGate.className}`}
         >
           HANG NADIM
         </h1>
         <p
-          className={`text-5xl text-gray-500 drop-shadow-xl ${warGate.className}`}
+          className={`text-2xl md:text-5xl text-gray-500 drop-shadow-xl ${warGate.className}`}
         >
           TANGKAS BERANI
         </p>
-      </div>
+      </RevealList>
     </section>
   );
 };
