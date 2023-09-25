2d977275f235216a1b5314af0698737204446daa
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-25 22:27:48 +0800

    :construction: Initiate about section

---
+++ app/sections/AboutScreen.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="w-full h-screen flex items-center justify-center bg-orange-700"
+    >
+      <h1>About</h1>
+    </section>
+  );
+};
+
+export default About;
