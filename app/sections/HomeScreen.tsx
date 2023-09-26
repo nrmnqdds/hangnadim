95c60a639685f096e427e96d0bd1bea4155887ab
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-26 11:16:28 +0800

    :lipstick: Change desc color

--- app/sections/HomeScreen.tsx
+++ app/sections/HomeScreen.tsx
@@ -1,25 +1,33 @@
-import localFont from "next/font/local";
+import { useState, useEffect } from "react";
 import { RevealList } from "next-reveal";
 
-const warGate = localFont({ src: "../../public/Wargate-Normal.otf" });
-
 const HomeScreen = () => {
+  const [blur, handleBlur] = useState<boolean>(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 1000) {
+        handleBlur(true);
+      } else handleBlur(false);
+    });
+  }, []);
   return (
     <section className="isolate w-full h-screen flex items-center justify-center">
-      <RevealList
-        interval={200}
-        delay={200}
-        easing="ease-in-out"
-        origin="bottom"
-        className="fixed text-center invisible"
-      >
-        <h1 className="text-5xl md:text-8xl text-slate-100 drop-shadow-glow">
-          HANG NADIM
-        </h1>
-        <p className="text-2xl md:text-5xl text-gray-500 drop-shadow-xl">
-          TANGKAS BERANI
-        </p>
-      </RevealList>
+      {blur ? null : (
+        <RevealList
+          interval={200}
+          delay={200}
+          easing="ease-in-out"
+          origin="bottom"
+          className="fixed text-center invisible"
+        >
+          <h1 className="text-5xl md:text-8xl text-slate-100 drop-shadow-glow">
+            HANG NADIM
+          </h1>
+          <p className="text-2xl md:text-5xl text-orange-500 drop-shadow-xl">
+            TANGKAS BERANI
+          </p>
+        </RevealList>
+      )}
     </section>
   );
 };
